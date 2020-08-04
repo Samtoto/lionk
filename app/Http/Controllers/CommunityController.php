b00272a6c0a2fbf6751908b4c9d9c0f4482e7dbf
Samtoto <lisensen163@gmail.com>
2020-08-04 19:24:30 +0800

    perf: Remove unused function CommunityController@my

--- app/Http/Controllers/CommunityController.php
+++ app/Http/Controllers/CommunityController.php
@@ -48,15 +48,6 @@ class CommunityController extends Controller
         return response()->json($communities, 200);
     }
 
-    public function my(Request $request)
-    {
-        $user = $request->user();
-        $myCommunities = User::where('id', $user->id)->with('community')->get()[0]->community;
-        return response()->json($myCommunities, 200);
-        // dump($myCommunities->toJson(JSON_PRETTY_PRINT));
-        // dump($user->toJson(JSON_PRETTY_PRINT));
-    }
-
     public function show(Request $request)
     {
         return view('community');
