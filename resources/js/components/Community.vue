e1d9a7768f7373629969307c59bb30c441c61a0b
Samtoto <lisensen163@gmail.com>
2020-08-07 10:18:07 +0800

    feat: disable highlight.js

--- resources/js/components/Community.vue
+++ resources/js/components/Community.vue
@@ -45,8 +45,8 @@
 <script>
 
 import marked from 'marked';
-import hljs from 'highlight.js';
-import "highlight.js/styles/tomorrow-night.css";
+// import hljs from 'highlight.js';
+// import "highlight.js/styles/tomorrow-night.css";
 
 
 // init marked
@@ -55,9 +55,9 @@ marked.setOptions({
     highlight: function(code, language) {
         // console.log(code, language)
         // const hljs = require('highlight.js');
-        const validLanguage = hljs.getLanguage(language) ? language : 'plaintext';
+        // const validLanguage = hljs.getLanguage(language) ? language : 'plaintext';
         // console.log(hljs.highlight(validLanguage, code).value);
-        return hljs.highlight(validLanguage, code).value;
+        // return hljs.highlight(validLanguage, code).value;
     },
     pedantic: false,
     gfm: true,
